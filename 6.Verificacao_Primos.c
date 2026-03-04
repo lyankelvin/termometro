97e4b2d5bc3398c6c922f16dd943eb145c8ed798
lyankelvin <lyankelin94@gmail.com>
2026-03-04 20:23:34 -0300

    Add files via upload

---
+++ 6.Verificacao_Primos.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int ehPrimo(int n) {
+
+    if (n <= 1) return 0;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return 0;
+    }
+
+    return 1;
+}
+
+int main() {
+
+    int num;
+
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    if (ehPrimo(num)){
+
+        printf("%d eh primo.\n", num);
+    }
+    else{
+        printf("%d nao eh primo.\n", num);
+    }
+
+}
